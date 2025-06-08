6cec89d523bfcf873298e62216bc9a01d0679fe4
Dewa <dewamadewira25@gmail.com>
2025-06-08 23:21:06 +0700

    update : start.bat for queue

--- start.bat
+++ start.bat
@@ -6,4 +6,8 @@ start "" php artisan serve
 echo Starting npm development server...
 start "" npm run dev
 
+
+echo Starting Job Queue...
+start "" php artisan queue:work
+
 echo All servers started successfully.
